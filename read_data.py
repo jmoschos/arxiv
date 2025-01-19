0ac7ac5ad1fc977a580aef4d76daab5a2ced9152
jmoschos <moschos.vangelis@gmail.com>
2025-01-19 16:22:48 +0100

    arxiv reading + result compilation

--- read_data.py
+++ read_data.py
@@ -1,8 +1,25 @@
-from datasets import load_dataset
+import arxiv
+import pandas as pd
 
 class DataReader:
-    def __init__(self, dataset_name):
-        self.dataset_name = dataset_name
+    def __init__(self, query, max_results=30, sort_by=arxiv.SortCriterion.SubmittedDate):
+        self.query = query
+        self.max_results = max_results
+        self.sort_by = sort_by
 
-    def load(self):
-        return load_dataset(self.dataset_name)
+    def query_arxiv(self):
+        return arxiv.Search(query=self.query, max_results=self.max_results, sort_by=self.sort_by)
+
+    def append_results(self):
+        self.papers = []
+        for result in self.search:
+            self.papers.append({
+                'published': result.published,
+                'title': result.title,
+                'abstract': result.summary,
+                'categories': result.categories
+            })
+
+    def create_dataset(self):
+        self.search = self.query_arxiv()
+        self.append_results()
